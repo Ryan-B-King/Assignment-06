8a6ac95db8659dc89b4476ae99f5f5bd748c222a
KING-PC\King <ryan13king@gmail.com>
2020-04-06 23:45:30 -0700

    Assignment 6 - in progress

--- js/dom-practice.js
+++ js/dom-practice.js
@@ -1,21 +1,95 @@
 /*eslint-env browser*/
+window.document.addEventListener("DOMContentLoaded", init);
+function init() {
 
-//STEP 1
+    //STEP 1
 
-//STEP 2
+    let button = document.querySelector("button");
+    button.addEventListener("click", () => {
+    window.alert("1: I have been clicked");
+    });
 
-//STEP 3
+    //STEP 2
 
-//STEP 4
+    let button2 = document.getElementById("button2");
+    button2.addEventListener("click", () => {
+    window.alert("2: I have been clicked");
+    });
 
-//STEP 5
+    //STEP 3
 
-//STEP 6
+    let button3 = document.getElementById("button3");
+    button3.addEventListener("click", function clickedBtn() {
+        window.alert("3: I have been clicked");
+    });
 
-//STEP 7
+    //STEP 4
 
-//STEP 8
+    let button4 = document.getElementById("button4");
+    function clickedBtn() {
+        window.alert("4: I have been clicked");
+    };
+    button4.addEventListener("click", clickedBtn);
 
-//STEP 9
+    //STEP 5
 
-//STEP 10
+    // DOMContentLoaded has been place on top of JS file
+    let button5 = document.getElementById("button5");
+    function clickedBtn2() {
+        window.alert("5: I have been clicked");
+    };
+    button5.addEventListener("click", clickedBtn2);
+
+    //STEP 6
+
+    let googleLink = document.getElementById("redirect");
+    function linkFail(e) {
+        e.preventDefault();
+        window.alert("You clicked a link to Google.com; however, you are denied");
+    };
+    googleLink.addEventListener("click", linkFail);
+
+    //STEP 7
+
+    let button7 = document.getElementById("button7");
+    let textBox = document.querySelector("textarea");
+    textBox.value = "Type something and I'll repeat it!"
+    function buttonFail(e) {
+        e.preventDefault();
+        window.alert(textBox.value);
+    };
+    button7.addEventListener("click", buttonFail);
+
+    //STEP 8
+
+    let button8 = document.getElementById("button8");
+    function clickedBtn3() {
+        window.open('newpage.html', 'newpage', 'width = 300px, height = 300px' );
+    };
+    button8.addEventListener("click", clickedBtn3);
+
+    //STEP 9
+
+    let button9a = document.getElementById("button9a");
+    let button9b = document.getElementById("button9b");
+    let activeInterval;
+
+    button9a.addEventListener("click", function messageJS() {
+        activeInterval = window.setInterval(function messageActive() {
+            window.console.log("Learning JavaScript is fun!");
+        }, 1000);
+    });
+
+    button9b.addEventListener("click", function killMessage(){
+        window.clearInterval(activeInterval);
+    });
+
+    //STEP 10
+
+
+
+
+
+
+
+};
---
+++ js/newpage.js
@@ -0,0 +1,11 @@
+window.document.addEventListener("DOMContentLoaded", init);
+function init() {
+
+    //STEP 8
+    
+    function delayLoad() {
+        window.resizeTo (500,300);
+    };
+    window.setTimeout(delayLoad, 2000);
+
+};
