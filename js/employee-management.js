f5ad00b22f64529f5132b0c8bcf590a3cea7005b
KING-PC\King <ryan13king@gmail.com>
2020-04-09 19:45:03 -0700

    updated

--- js/employee-management.js
+++ js/employee-management.js
@@ -1,8 +1,53 @@
 // /*eslint-env browser*/
-// window.document.addEventListener("DOMContentLoaded", init);
-// function init() {
-//     "use strict";
-    // function getEmployees() {
+window.document.addEventListener("DOMContentLoaded", init);
+function init() {
+    "use strict";
+
+    function main() {
+        "use strict";
+        // let employee_list;
+        let row = 1;
+        let addBtn = document.getElementById("addBtn");
+
+        // employee_list = getEmployees();
+
+        addBtn.addEventListener("click", addRow);
+
+        function addRow() {
+            "use strict";
+
+            let name = document.getElementById("empName").value;
+            let title = document.getElementById("empTitle").value;
+            let extension = document.getElementById("extension").value;
+
+            if (!name || !title || !extension) {
+                window.alert("Please fill all the boxes");
+                return;
+            };
+
+            let empTable = document.getElementById("empTable");
+            let newRow = empTable.insertRow(row);
+            let cell1 = newRow.insertCell(0);
+            let cell2 = newRow.insertCell(1);
+            let cell3 = newRow.insertCell(2);
+            let cell4 = newRow.insertCell(3);
+
+            cell1.innerHTML = name;
+            cell2.innerHTML = title;
+            cell3.innerHTML = extension;
+            cell4.innerHTML = "Delete";
+
+            row++;
+        };
+
+        // display(employee_list);
+        
+    };
+    main();
+};
+
+
+// function getEmployees() {
     //     "use strict";
     //     let employee_list = [];
     //     employee_list[0] = ["Chris Martin", "Web Developer", 1739];
@@ -24,48 +69,3 @@
     //     });
     //     window.console.log("");
     // };
-
-    // function main() {
-        // "use strict";
-        // let employee_list;
-        let row = 1;
-        let addBtn = document.getElementById("addBtn");
-
-        // employee_list = getEmployees();
-
-        addBtn.addEventListener("click", addRow);
-
-        function addRow() {
-            "use strict";
-
-            let name = document.getElementById("empName").value;
-            let title = document.getElementById("empTitle").value;
-            let extension = document.getElementById("extension").value;
-
-            // if (!name || !title || !extension) {
-            //     window.alert("Please fill all the boxes");
-            //     return;
-            // };
-
-            let empTable = document.getElementById("empTable");
-            let newRow = empTable.insertRow(1);
-            let cell1 = newRow.insertCell(0);
-            let cell2 = newRow.insertCell(1);
-            let cell3 = newRow.insertCell(2);
-            let cell4 = newRow.insertCell(3);
-
-            cell1.innerHTML = name;
-            cell2.innerHTML = title;
-            cell3.innerHTML = extension;
-            cell4.innerHTML = "Delete";
-
-            row++;
-        };
-
-        // display(employee_list);
-        
-    // };
-    // main();
-// };
-
-
